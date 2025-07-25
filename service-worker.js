6f655275f082f79facbe0e06d821fc93f54da0c3
DanBudszzz <tdanbuds@gmail.com>
2025-07-25 10:10:24 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,22 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('pwa-nclexbuddy-cache').then((cache) => {
+      console.log('Opened cache');
+      // More explicit and reliable list of files to cache
       return cache.addAll([
-          '.',
-          'index.html',
-          'https://raw.githubusercontent.com/DanBudszzz/PWAtest/main/icon.png'
-        ]);
+        './', // Caches the root of your site.
+        'index.html', // Your main HTML file.
+        'https://raw.githubusercontent.com/DanBudszzz/PWAtest/main/icon.png' // Cross-origin resource.
+      ]);
     })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      // If the request is in the cache, return it. Otherwise, fetch from the network.
+      return response || fetch(e.request);
+    })
   );
 });
